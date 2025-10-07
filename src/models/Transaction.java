19dcfdfc226eb14ffe3e7cad7eeac44d4a9ce7d9
Emmanuel <Emmanuel@LAPTOP-LTCPIV11>
2025-10-07 19:37:42 +0100

    Transaction

--- src/models/Transaction.java
+++ src/models/Transaction.java
@@ -6,4 +6,42 @@ import java.time.format.DateTimeFormatter;
 public class Transaction {
     private double amount;
     private String type;
+    private String description;
+    private LocalDateTime date;
+    private String category;
+
+    public Transaction(double amount, String type, String description, String category){
+        this.amount = amount;
+        this.type = type;
+        this.description = description;
+        this.category = category;
+        this.date = LocalDateTime.now();
+    }
+
+    //Getters and Seetters
+    public double getAmount() { return amount;}
+    public String getType() {return type;}
+    public String getDescription() {return description;}
+    public LocalDateTime getData() {return date;}
+    public String getCategory() {return category;}
+
+    //toString for easy printing
+    @Override
+    public String toString(){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        return String.format("%s,%s,%.2f,%s,%s", date.format(formatter),type,amount,description,date,category);
+    }
+
+    //From CSV string (for Loading)
+    public static Transaction fromCSV(String csvLine){
+        String[] parts = csvLine.split(",");
+        LocalDateTime date = LocalDateTime.parse(parts[0], DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
+        String type = parts[1];
+        double amount = Double.parseDouble(parts[2]);
+        String description = parts[3];
+        String category = parts.length > 4 ? parts[4] : "";
+        Transaction t = new Transaction(amount, type, description, category);#
+        t.date = date; // Override Date
+        return t;
+    }
 }
